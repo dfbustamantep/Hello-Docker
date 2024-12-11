cd0c4d228cd92141bbfee33733ea02a3dd7f003e
Daniel Bustamante <vtdanielf@gmail.com>
2024-12-10 20:03:55 -0500

    UPDATE README

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://daniel:password@monguito:27017/miapp?authSource=admi
 
 //ruta raiz 
 app.get('/',async(_req,res)=>{
-    console.log('listando... chanchitos')
+    console.log('listando...')
     const animales = await Animal.find();
     return res.send(animales)
 })
